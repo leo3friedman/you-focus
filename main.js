c25ff68f2fae69562a6cdb8a979efb1ec05f6d04
Leo <leo@fourfriedmands.com>
2020-10-30 13:03:03 -0700

    option actually change YT

--- main.js
+++ main.js
@@ -29,10 +29,17 @@ function showElement(elementSelector, display) {
   }
 }
 // hideElement("body");
+const defaultSettings = {
+  hideHomepageVideos: true,
+  hideHomepageSidebar: true,
+  hidePlayerEndwall: true,
+  hidePlayerRelated: true,
+  hidePlayerComments: false,
+  isShowing: true,
+  adShowing: true,
+};
 window.onload = function () {
-  chrome.storage.sync.get({ isShowing: true, adShowing: true }, function (
-    result
-  ) {
+  chrome.storage.sync.get(defaultSettings, function (result) {
     if (result.isShowing === false) {
       hideDistractions();
       disableAutoplay();
@@ -43,8 +50,14 @@ window.onload = function () {
     // Special case because it was popping up on refresh
     showElement("#guide-content");
     showElement("body", "block");
+    updateHomepageSidebarClass(result.hideHomepageSidebar);
+    updateHomepageVideosClass(result.hideHomepageVideos);
+    updateVideoplayerRelatedClass(result.hidePlayerRelated);
+    updateVideoplayerEndwallClass(result.hidePlayerEndwall);
+    updateVideoplayerCommentsClass(result.hidePlayerComments);
   });
 };
+
 chrome.storage.onChanged.addListener(function (changes, areaName) {
   if (changes.isShowing !== undefined) {
     if (changes.isShowing.newValue === false) {
@@ -62,5 +75,56 @@ chrome.storage.onChanged.addListener(function (changes, areaName) {
     }
   }
 });
-
+chrome.storage.onChanged.addListener(function (changes, areaName) {
+  if (changes.hideHomepageSidebar) {
+    updateHomepageSidebarClass(changes.hideHomepageSidebar.newValue);
+  }
+  if (changes.hideHomepageVideos) {
+    updateHomepageVideosClass(changes.hideHomepageVideos.newValue);
+  }
+  if (changes.hidePlayerRelated) {
+    updateVideoplayerRelatedClass(changes.hidePlayerRelated.newValue);
+  }
+  if (changes.hidePlayerEndwall) {
+    updateVideoplayerEndwallClass(changes.hidePlayerEndwall.newValue);
+  }
+  if (changes.hidePlayerComments) {
+    updateVideoplayerCommentsClass(changes.hidePlayerComments.newValue);
+  }
+});
+function updateHomepageSidebarClass(val) {
+  if (val) {
+    document.body.classList.add("homepage-sidebar-hide");
+  } else {
+    document.body.classList.remove("homepage-sidebar-hide");
+  }
+}
+function updateHomepageVideosClass(val) {
+  if (val) {
+    document.body.classList.add("homepage-videos-hide");
+  } else {
+    document.body.classList.remove("homepage-videos-hide");
+  }
+}
+function updateVideoplayerRelatedClass(val) {
+  if (val) {
+    document.body.classList.add("videoplayer-related-hide");
+  } else {
+    document.body.classList.remove("videoplayer-related-hide");
+  }
+}
+function updateVideoplayerEndwallClass(val) {
+  if (val) {
+    document.body.classList.add("videoplayer-endwall-hide");
+  } else {
+    document.body.classList.remove("videoplayer-endwall-hide");
+  }
+}
+function updateVideoplayerCommentsClass(val) {
+  if (val) {
+    document.body.classList.add("videoplayer-comments-hide");
+  } else {
+    document.body.classList.remove("videoplayer-comments-hide");
+  }
+}
 // functions for options
