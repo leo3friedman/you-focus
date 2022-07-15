4f4451d4cee61e189a6c4953372a272586236fed
leo3friedman <73138942+leo3friedman@users.noreply.github.com>
2022-07-15 12:05:26 -0700

    shorten main.js

--- main.js
+++ main.js
@@ -1,18 +1,3 @@
-function showDistractions() {
-  document.body.classList.remove("yf-hide");
-}
-
-function hideDistractions() {
-  document.body.classList.add("yf-hide");
-}
-
-function showElement(elementSelector, display) {
-  let selectedElement = document.querySelector(elementSelector);
-  if (selectedElement) {
-    selectedElement.style.visibility = "visible";
-  }
-}
-// hideElement("body");
 const defaultSettings = {
   hideMode: false,
   hideHomepageVideos: true,
@@ -22,101 +7,30 @@ const defaultSettings = {
   hidePlayerComments: false,
   isShowing: true,
 };
-window.onload = function () {
-  chrome.storage.sync.get(defaultSettings, function (result) {
-    // if (result.isShowing === false) {
-    //   hideDistractions();
-    // }
-    alterVisibility("hideMode", result.hideMode)
-    updateHomepageSidebarClass(result.hideHomepageSidebar);
-    updateHomepageVideosClass(result.hideHomepageVideos);
-    updateVideoplayerRelatedClass(result.hidePlayerRelated);
-    updateVideoplayerEndwallClass(result.hidePlayerEndwall);
-    updateVideoplayerCommentsClass(result.hidePlayerComments);
-    // Special case because it was popping up on refresh
-    showElement("#guide-content");
-    showElement("body", "block");
-  });
-};
-
-// chrome.storage.onChanged.addListener(function (changes, areaName) {
-//   if (changes.isShowing !== undefined) {
-//     if (changes.isShowing.newValue === false) {
-//       hideDistractions();
-//     } else {
-//       showDistractions();
-//     }
-//   }
-// });
+/**
+ * Hides/shows certain elements on the page by adding/removing classes to the page. Refer to hide.css to see what added
+ * classes hide what elements.
+ * @param    {String} change        Class to add/remove to page
+ * @param    {Boolean} newValue     True to add class, false to remove
+ */
 function alterVisibility(change, newValue) {
   newValue ? document.body.classList.add(change) : document.body.classList.remove(change);
 }
 
+window.onload = function () {
+  chrome.storage.sync.get(defaultSettings, function (result) {
+    Object.entries(result).forEach((el)=>{
+      alterVisibility(el[0],el[1])
+    })
+    // Special case because it was popping up on refresh (hide.css is hiding these initially)
+    document.querySelector("body").style.visibility = "visible"
+    document.querySelector("#guide-content").style.visibility = "visible"
+  });
+};
+
 chrome.storage.onChanged.addListener(function (changes, areaName) {
-  if (changes.isShowing !== undefined) {
-    changes.isShowing.newValue ? showDistractions() : hideDistractions();
-  }
-  if(changes.hideMode){
-    const change = Object.keys(changes)[0]
-    console.log(change)
-    const newValue = changes[change].newValue
-    alterVisibility(change, newValue)
-  }
-
-  // console.log(change)
-  // console.log(newValue)
-
-  // alterVisibility(change, newValue);
-
-
-  if (changes.hideHomepageSidebar) {
-    updateHomepageSidebarClass(changes.hideHomepageSidebar.newValue);
-  }
-  if (changes.hideHomepageVideos) {
-    updateHomepageVideosClass(changes.hideHomepageVideos.newValue);
-  }
-  if (changes.hidePlayerRelated) {
-    updateVideoplayerRelatedClass(changes.hidePlayerRelated.newValue);
-  }
-  if (changes.hidePlayerEndwall) {
-    updateVideoplayerEndwallClass(changes.hidePlayerEndwall.newValue);
-  }
-  if (changes.hidePlayerComments) {
-    updateVideoplayerCommentsClass(changes.hidePlayerComments.newValue);
-  }
+  const change = Object.keys(changes)[0]
+  const newValue = changes[change].newValue
+  alterVisibility(change, newValue)
 });
-function updateHomepageSidebarClass(val) {
-  if (val) {
-    document.body.classList.add("homepage-sidebar-hide");
-  } else {
-    document.body.classList.remove("homepage-sidebar-hide");
-  }
-}
-function updateHomepageVideosClass(val) {
-  if (val) {
-    document.body.classList.add("homepage-videos-hide");
-  } else {
-    document.body.classList.remove("homepage-videos-hide");
-  }
-}
-function updateVideoplayerRelatedClass(val) {
-  if (val) {
-    document.body.classList.add("videoplayer-related-hide");
-  } else {
-    document.body.classList.remove("videoplayer-related-hide");
-  }
-}
-function updateVideoplayerEndwallClass(val) {
-  if (val) {
-    document.body.classList.add("videoplayer-endwall-hide");
-  } else {
-    document.body.classList.remove("videoplayer-endwall-hide");
-  }
-}
-function updateVideoplayerCommentsClass(val) {
-  if (val) {
-    document.body.classList.add("videoplayer-comments-hide");
-  } else {
-    document.body.classList.remove("videoplayer-comments-hide");
-  }
-}
+
