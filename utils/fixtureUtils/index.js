8ac06545efc94d768dcc056d79ae11aecc3dd87b
Greg Friedman <greg@upallnightconsulting.com>
2026-07-10 11:22:26 -0700

    cleanup

--- utils/fixtureUtils/index.js
+++ utils/fixtureUtils/index.js
@@ -1,5 +1,6 @@
 /**
- * fixtureUtils provides methods for creating a static website test fixture from a website that has
+ * @module fixtureUtils
+ * provides methods for creating a static website test fixture from a website that has
  *  been saved using Chrome > File > Save Page As...
  */
 export * from './omitScriptTags.js'
